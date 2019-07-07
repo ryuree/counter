aa43c36440a8a6a2aa358b4a7118e57d5ec05358
ryuree <rido94@naver.com>
2019-07-07 10:23:49 +0900

    [ADD] WEBPACK CONFIG

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    stats: {
+      color: true,
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ProgressBarPlugin({
+      format: 'Build [:bar] :percent (:elapsed seconds)',
+      clear: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+};
